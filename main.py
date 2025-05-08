756c1f59226c8a76c41ec74b837f76e64c923f0f
Jamaludin <74maludin@gmail.com>
2025-05-09 03:24:09 +0700

    Add initial project files and resource monitoring script
    - Created .env.example for environment variable configuration
    - Added .gitignore to exclude Python-generated files and virtual environments
    - Defined Python version in .python-version
    - Updated README.md with project description
    - Implemented resource monitoring in main.py using psutil
    - Configured dependencies in pyproject.toml and uv.lock

---
+++ main.py
@@ -0,0 +1,78 @@
+import psutil
+import requests
+import os
+from datetime import datetime
+from dotenv import load_dotenv
+
+# activate read from .env
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+TITLE = os.getenv("TITLE") or "Server"
+CPU_THRESHOLD = os.getenv("CPU_THRESHOLD") or 80
+MEMORY_THRESHOLD = os.getenv("MEMORY_THRESHOLD") or 80
+
+
+def monitor_resources():
+    msg = f"*{TITLE}*\n\n"
+
+    # Warm-up call to set a baseline
+    for proc in psutil.process_iter(["pid", "name", "username", "cpu_percent"]):
+        pass
+
+    # Checking CPU, Memory, Disk Usage
+    cpu_usage = psutil.cpu_percent(interval=1)
+    memory_usage = psutil.virtual_memory().percent
+    disk_usage = psutil.disk_usage("/").percent
+
+    print(cpu_usage, memory_usage)
+
+    msg += f"CPU Usage: {cpu_usage}%\n"
+    msg += f"Memory Usage: {memory_usage}%\n"
+    msg += f"Disk Usage: {disk_usage}%\n\n"
+
+    # Top 5 CPU Processing
+    processes = []
+    for proc in psutil.process_iter(["pid", "name", "username", "cpu_percent"]):
+        processes.append(proc.info)
+
+    cpu_sorted = sorted(processes, key=lambda p: p["cpu_percent"] or 0.0, reverse=True)
+
+    msg += "*Top 5 CPU consuming processes:*\n"
+    for process in cpu_sorted[:5]:
+        cpu_percent = process["cpu_percent"] or 0.0
+        msg += f"PID: {process['pid']}, Name: {process['name']},  CPU: {cpu_percent:.2f}%\n"
+    msg += "\n"
+
+    # Top 5 Memory Processing
+    processes = []
+    for proc in psutil.process_iter(["pid", "name", "username", "memory_percent"]):
+        try:
+            processes.append(proc.info)
+        except (psutil.NoSuchProcess, psutil.AccessDenied):
+            continue
+
+    mem_sorted = sorted(
+        processes, key=lambda p: p["memory_percent"] or 0.0, reverse=True
+    )
+
+    msg += "*Top 5 memory-consuming processes:\n*"
+    for process in mem_sorted[:5]:
+        mem_percent = process["memory_percent"] or 0.0
+        msg += f"PID: {process['pid']}, Name: {process['name']}, Memory: {mem_percent:.2f}%\n"
+
+    if cpu_usage > float(CPU_THRESHOLD) or memory_usage > float(MEMORY_THRESHOLD):
+        success = send_to_telegram(msg)
+        print("Sent to Telegram:", success, datetime.now())
+
+
+def send_to_telegram(message):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    payload = {"chat_id": CHAT_ID, "text": message, "parse_mode": "Markdown"}
+    r = requests.post(url, data=payload)
+    return r.ok
+
+
+if __name__ == "__main__":
+    monitor_resources()
